bdbb86fb6d76bf3620d5115f24e49b060da04899
unknown <harsh.snvp.hc@gmail.com>
2022-10-30 19:48:51 +0530

    First Project:Zoom Clone

---
+++ server.js
@@ -0,0 +1,46 @@
+const express=require("express")
+const app=express()
+const server=require("http").createServer(app)
+const { v4: uuidV4 } = require('uuid')
+const io = require('socket.io')(server)
+app.use(express.static("public"))
+app.set("view engine","ejs")
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true
+});
+app.use('/peerjs', peerServer);
+
+
+app.get("/", (req,res)=>{
+  res.redirect(`/${uuidV4()}`)
+})
+
+app.get("/:room",(req,res)=>{
+  res.render('rooms', { roomId: req.params.room })
+})
+
+io.on('connection', function(socket) {
+  socket.on("join-room",(roomId,userId)=>{
+    console.log("New User connected:",userId),
+    socket.join(roomId)
+    socket.broadcast.emit('user-connected', userId);
+  
+ 
+   socket.on("message",(message)=>{
+    io.to(roomId).emit('createMessage', message, userId)
+
+   }) 
+
+
+   socket.on('disconnect', () => {
+    console.log("user disconnected:",userId)
+    socket.to(roomId).emit('user-disconnected', userId)
+     })
+  })
+
+});
+
+server.listen(3030,()=>{
+  console.log("server is listening")
+}) 
